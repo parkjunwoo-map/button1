3d18539610f37b5052b3ec3bb34b351548c37b43
박준우 <a90846251@gmail.com>
2024-09-23 11:15:08 +0900

    Updated button text to name and student ID

--- button1.py
+++ button1.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Yeon Ryu")
-b2 = Button(window, text="60221010")
+b1 = Button(window, text="박준우")
+b2 = Button(window, text="60232849")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
